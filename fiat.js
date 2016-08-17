9ec4fc2cb21b791e599b1b28a25c2a512dc03b0c
fabiano1976 <fabiano1976@gmail.com>
2016-08-17 01:27:06 -0300

    new fiat feat

---
+++ fiat.js
@@ -0,0 +1,16 @@
+var fiat = " eur usd brl cny ars cad gbp jpy rub ";
+var fiax = " eux usx brx cnx arsx cax gbx jpx rux ";
+var fiaz =  fiat.concat(fiax);
+
+
+var pair = "btceur";
+var paix = "btceax";
+
+var sstr = paix.slice(3, 6);
+
+if(fiaz.indexOf(sstr) > -1){
+	console.log(sstr +' present');
+}else{
+	console.log(sstr +' not present');
+}
+
---
+++ moneytest.js
@@ -0,0 +1,23 @@
+var oxr = require('open-exchange-rates');
+var fx = require('money');
+var toeur = {}; 
+
+oxr.set({ app_id: 'dcd70ee49fad49bfb7f26d1bce5f0bf0' })
+
+oxr.latest(function() {
+    // Apply exchange rates and base rate to `fx` library object:
+    fx.rates = oxr.rates;
+    fx.base = oxr.base;
+
+    toeur['usd']  = fx(1).from("USD").to("EUR");
+    toeur['brl']  = fx(1).from("BRL").to("EUR");
+    toeur['cny']  = fx(1).from("CNY").to("EUR");
+    toeur['ars']  = fx(1).from("ARS").to("EUR");
+    toeur['cad']  = fx(1).from("CAD").to("EUR");
+    toeur['gbp']  = fx(1).from("GBP").to("EUR");
+    toeur['jpy']  = fx(1).from("JPY").to("EUR");
+    toeur['rub']  = fx(1).from("RUB").to("EUR");
+
+    console.log(toeur);
+});
+
